0a5cd8e040b6341594c4c447e2363a6b8a543672
junyeoble <junyeoble@gmail.com>
2021-07-15 15:05:27 -0700

    implemented get user api route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,20 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
 router.post(
   '/',
   asyncHandler(async (req, res, next) => {
