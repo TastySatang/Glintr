63a286df32e146de2d465da749911dea2ffbe03f
junyeoble <junyeoble@gmail.com>
2021-07-16 13:08:45 -0700

    implemented LoginForm

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom';
+
+import './LoginForm.css'
+import * as sessionActions from '../../store/session';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user)
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return <Redirect to="/" />
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error) => <li key={error}>{error}</li>)}
+      </ul>
+      <label>
+        Username or Email
+        <input
+          type='text'
+          value={credential}
+          onchange={(e) => setCredential(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Log in</button>
+    </form>
+  );
+}
+
+export default LoginFormPage;
