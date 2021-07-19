ecf912ecbe6ab553ae036f31eec1739e11dff3ec
junyeoble <junyeoble@gmail.com>
2021-07-19 16:26:12 -0700

    albumId nolonger required for images

--- backend/db/migrations/20210719204431-create-image.js
+++ backend/db/migrations/20210719204431-create-image.js
@@ -14,7 +14,6 @@ module.exports = {
         references: { model: "Users" }
       },
       albumId: {
-        allowNull: false,
         type: Sequelize.INTEGER,
         references: { model: "Albums" }
       },
